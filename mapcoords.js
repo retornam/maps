ca61281bfbabfe2ca5dcf8c29577b442f19104c0
retornam <retornam@users.noreply.github.com>
2014-12-26 15:36:16 -0800

    finishing touches

--- mapcoords.js
+++ mapcoords.js
@@ -1,5 +1,6 @@
- var map = L.mapbox.map('map', 'retornam.ionbikm0')
+ var map = L.mapbox.map('map', 'retornam.700dd677')
                 .setView([37.545, -122.482], 10);
             var featureLayer = L.mapbox.featureLayer()
                 .loadURL('jsoncoords.json')
                 .addTo(map);
+                // retornam.700dd677 retornam.ionbikm0
